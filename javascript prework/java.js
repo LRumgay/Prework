0f8410d48fe0a36d91798c6f3d2bb4c82897bcc3
Liam Rumgay <Liamrumgay2@gmail.com>
2021-10-07 00:10:53 -0500

    added missing files

---
+++ javascript prework/java.js
@@ -0,0 +1,17 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height = "250px";
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box").style.backgroundColor = "blue";
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+    document.getElementById("box").style.height = "0px";
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+    document.getElementById("box").style.height = "120px";
+});
+
+
